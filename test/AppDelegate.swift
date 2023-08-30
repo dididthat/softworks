312fb070c214caddd69478bc6b3aec98a1b85c22
dididthat <diananikulina00@mail.ru>
2023-08-30 16:25:18 +0300

    Initial Commit

---
+++ test/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by Diana Nikulina on 30.08.2023.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
